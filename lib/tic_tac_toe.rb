3566dd3cbf91fb212c8db0652119e8d991944807
Min Kim <mk4555@nyu.edu>
2017-10-05 04:13:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,68 +9,68 @@ class TicTacToe
     @board = [" "," "," "," "," "," "," "," "," "]
   end
 
-  def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
   def input_to_index(input)
     input.to_i - 1
   end
 
-  def move(board, index, token)
-    board[index] = token
+  def move(index, token)
+    @board[index] = token
   end
 
-  def position_taken?(board, index)
-    !(board[index] == " " || board[index] == "" || board[index] == nil)
+  def position_taken?(index)
+    !(@board[index] == " " || @board[index] == "" || @board[index] == nil)
   end
 
-  def valid_move?(board, index)
+  def valid_move?(index)
     if index.between?(0,8)
-      !position_taken?(board, index)
+      !position_taken?(index)
     end
   end
 
-  def turn(board)
+  def turn
     puts "Enter number 1-9:"
     input = gets.chomp
     index = input_to_index(input)
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
+    if valid_move?(index)
+      move(index, current_player)
     else
       turn(board)
     end
   end
 
-  def turn_count(board)
+  def turn_count
     counter = 0
-    board.each do |position|
+    @board.each do |position|
       counter += 1 if position == "X" || position == "O"
     end
     counter
   end
 
-  def current_player(board)
-    if turn_count(board) % 2 == 0
+  def current_player
+    if turn_count % 2 == 0
       "X"
     else
       "O"
     end
   end
 
-  def won?(board)
+  def won?
     WIN_COMBINATIONS.each do |combo|
       win_index_1 = combo[0]
       win_index_2 = combo[1]
       win_index_3 = combo[2]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
 
       if (position_1 == "X" && position_2 == "X" && position_3 == "X")
         return combo
@@ -81,13 +81,13 @@ class TicTacToe
     false
   end
 
-  def full?(board)
-    !board.any?{|position| position == " "}
+  def full?
+    !@board.any?{|position| position == " "}
   end
 
-  def draw?(board)
-    if full?(board)
-      if won?(board)
+  def draw?
+    if full?
+      if won?
         return false
       else
         return true
@@ -96,24 +96,24 @@ class TicTacToe
     false
   end
 
-  def over?(board)
-    full?(board) || won?(board) || draw?(board)
+  def over?
+    full?(@board) || won?(@board) || draw?(@board)
   end
 
-  def winner(board)
-    if won?(board)
-      board[won?(board)[0]]
+  def winner
+    if won?
+      @board[won?[0]]
     end
   end
 
-  def play(board)
-    while !over?(board) do
-      display_board(board)
-      turn(board)
+  def play
+    while !over? do
+      display_board
+      turn
       # puts "count: #{counter} "
     end
-    if won?(board)
-      puts "Congratulations #{winner(board)}!"
+    if won?
+      puts "Congratulations #{winner}!"
     else
       puts "Cat's Game!"
     end
